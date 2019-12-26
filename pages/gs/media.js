c55eb70d1d076e0b975678bff8fe2468f5b67fd0
Tom Farmer <tfarmer@hamamatsu.co.uk>
2019-12-26 00:15:53 +0000

    Improved gs images and text

--- pages/gs/media.js
+++ pages/gs/media.js
@@ -1,1 +1,1 @@
-let media=['media/00_globe_terrain.png','media/10_globe_biomes.png','media/20_globe_river_basins.png','media/30_globe_textures.png',]
+let media=['media/00_globe_terrain.png','media/10_globe_biomes.png','media/30_globe_textures.png',]
