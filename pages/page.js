5b277bd83ad91cd923fa208c00aa21bd0d81c4cb
Tom Farmer <farmergeddon@gmail.com>
2020-01-07 21:57:11 +0000

    Revert "Replaced software renderer videos with gif"
    This reverts commit 18bbcedff7448ec21325cb00b73ed7e30a958f58.

--- pages/page.js
+++ pages/page.js
@@ -22,10 +22,35 @@ function updateMediaContainer() {
 
     mediaContainer.innerHTML = "";
 
-	var image = document.createElement("img");
-	image.setAttribute("src", filename);
-	image.classList.add("project-page-image");
-	mediaContainer.appendChild(image)
+    if (filename.endsWith("png")) {
+        var image = document.createElement("img");
+        image.setAttribute("src", filename);
+        image.classList.add("project-page-image");
+        mediaContainer.appendChild(image)
+    }
+    else if (filename.endsWith("mp4")) {
+        var video = document.createElement("video");
+		video.setAttribute("controls", "controls");
+		
+		var source = document.createElement("source");
+		source.setAttribute("src", filename);
+		source.setAttribute("type", "video/mp4");
+		video.appendChild(source);
+		
+		source = document.createElement("source");
+		source.setAttribute("src", filename.replace(".mp4", ".ogg"));
+		source.setAttribute("type", "video/ogg");
+		video.appendChild(source);
+		
+		source = document.createElement("source");
+		source.setAttribute("src", filename.replace(".mp4", ".webm"));
+		source.setAttribute("type", "video/webm");
+		video.appendChild(source);
+		
+		video.classList.add("project-page-image");
+		
+		mediaContainer.appendChild(video);
+    }
 }
 
 function previousMedia() {
